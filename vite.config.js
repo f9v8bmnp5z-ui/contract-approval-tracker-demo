5b2f62d9119ccebfd76f53858cb3061c50eb00b4
f9v8bmnp5z-ui <f9v8bmnp5z@privaterelay.appleid.com>
2026-09-15 19:26:52 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// If you deploy to GitHub Pages at https://<user>.github.io/<repo>/,
-// uncomment and set `base` to "/<repo>/". Vercel/Netlify don't need this.
-export default defineConfig({
-  plugins: [react()],
-  // base: "/contract-tracker-app/",
-});
